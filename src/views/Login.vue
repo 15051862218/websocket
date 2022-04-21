96c1b54e86797d1cd5109dee3cff83a12abcae78
sunshine <2531227817@qq.com>
2022-04-21 13:09:22 +0800

    websocket双工通信弹幕组件练习

---
+++ src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="login">
+    <input type="text" placeholder="请输入用户名" v-model="username"/>
+    <button @click="handleBtnClick">进入聊天室</button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Login',
+  data(){
+    return{
+      username:'',
+    }
+  },
+  methods:{
+    handleBtnClick(){
+      const username = this.username.trim();
+      if(username!='syk'){
+        alert('用户不存在！');
+        return;
+      }else{
+      localStorage.setItem('username',username);
+      this.$router.push('/');
+      }
+      
+    }
+  },
+
+  components: {
+   
+  }
+}
+</script>
